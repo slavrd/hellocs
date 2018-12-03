3773669d1103158627a22ce511c31684d5e9f01d
Slav Donchev <srd@Slavs-MacBook-Pro.local>
2018-12-03 15:29:07 +0200

    add hello function

--- hellocs/hello.cs
+++ hellocs/hello.cs
@@ -2,11 +2,15 @@
 
 namespace hellocs
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine(ReturnHello());
+        }
+
+        public static string ReturnHello () {
+            return "hello";
         }
     }
 }
