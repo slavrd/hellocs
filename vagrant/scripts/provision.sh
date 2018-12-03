5100266571d505bb966c2f4eac117c6475381940
Slav Donchev <srd@Slavs-MacBook-Pro.local>
2018-12-03 17:25:44 +0200

    edit provision script to download project

--- vagrant/scripts/provision.sh
+++ vagrant/scripts/provision.sh
@@ -12,3 +12,7 @@ sudo apt-get install -y apt-transport-https dotnet-sdk-2.1 git
 # clean up
 sudo apt-get clean
 rm /tmp/packages-microsoft-prod.deb
+
+# download the project
+git clone https://github.com/slavrd/hellocs.git
+sudo chown -R vagrant hellocs/
